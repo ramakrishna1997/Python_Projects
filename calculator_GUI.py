e9b660d7abdd70429764b1f0e997f283c10a1665
KILLI RAMA KRISHNA <84118950+ramakrishna1997@users.noreply.github.com>
2023-07-30 20:23:12 +0530

    Add files via upload

---
+++ calculator_GUI.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+calculation=""
+def add_to_calculation(symbol):
+    global calculation
+    calculation += str(symbol)
+    text_result.delete(1.0,'end')
+    text_result.insert(1.0,calculation)
+def evaluation_calculation():
+    global calculation
+    try:
+        calculation= str(eval(calculation))
+        text_result.delete(1.0,'end')
+        text_result.insert(1.0,calculation)
+    except:
+        clear_field()
+        text_result.insert(1.0,'Error')    
+def clear_field():
+    global calculation
+    calculation=""
+    text_result.delete(1.0,'end')
+root=tk.Tk()
+root.geometry("300x275")
+root.title("calculator") 
+root.iconbitmap(r'C:\Users\killi\Desktop\Rama Krishna\GUI_application\calculator.ico')
+text_result=tk.Text(root,height=2,width=16,font=('Arial',24))
+text_result.grid(columnspan=5)
+bnt_1=tk.Button(root,text='1',command=lambda:add_to_calculation(1),width=5,font=('Arial',14))
+bnt_1.grid(row=2,column=1)
+bnt_2=tk.Button(root,text='2',command=lambda:add_to_calculation(2),width=5,font=('Arial',14))
+bnt_2.grid(row=2,column=2)
+bnt_3=tk.Button(root,text='3',command=lambda:add_to_calculation(3),width=5,font=('Arial',14))
+bnt_3.grid(row=2,column=3)
+bnt_4=tk.Button(root,text='4',command=lambda:add_to_calculation(4),width=5,font=('Arial',14))
+bnt_4.grid(row=3,column=1)
+bnt_5=tk.Button(root,text='5',command=lambda:add_to_calculation(5),width=5,font=('Arial',14))
+bnt_5.grid(row=3,column=2)
+bnt_6=tk.Button(root,text='6',command=lambda:add_to_calculation(6),width=5,font=('Arial',14))
+bnt_6.grid(row=3,column=3)
+bnt_7=tk.Button(root,text='7',command=lambda:add_to_calculation(7),width=5,font=('Arial',14))
+bnt_7.grid(row=4,column=1)
+bnt_8=tk.Button(root,text='8',command=lambda:add_to_calculation(8),width=5,font=('Arial',14))
+bnt_8.grid(row=4,column=2)
+bnt_9=tk.Button(root,text='9',command=lambda:add_to_calculation(9),width=5,font=('Arial',14))
+bnt_9.grid(row=4,column=3)
+bnt_0=tk.Button(root,text='0',command=lambda:add_to_calculation(0),width=5,font=('Arial',14))
+bnt_0.grid(row=5,column=2)
+bnt_plus=tk.Button(root,text='+',command=lambda:add_to_calculation('+'),width=5,font=('Arial',14))
+bnt_plus.grid(row=2,column=4)
+bnt_minus=tk.Button(root,text='-',command=lambda:add_to_calculation('-'),width=5,font=('Arial',14))
+bnt_minus.grid(row=3,column=4)
+bnt_mul=tk.Button(root,text='*',command=lambda:add_to_calculation('*'),width=5,font=('Arial',14))
+bnt_mul.grid(row=4,column=4)
+bnt_div=tk.Button(root,text='/',command=lambda:add_to_calculation('/'),width=5,font=('Arial',14))
+bnt_div.grid(row=5,column=4)
+bnt_open=tk.Button(root,text='(',command=lambda:add_to_calculation('('),width=5,font=('Arial',14))
+bnt_open.grid(row=5,column=1)
+bnt_close=tk.Button(root,text=')',command=lambda:add_to_calculation(')'),width=5,font=('Arial',14))
+bnt_close.grid(row=5,column=3)
+bnt_C=tk.Button(root,text='C',command=clear_field,width=5,font=('Arial',14))
+bnt_C.grid(row=6,column=1)
+bnt_equal=tk.Button(root,text='=',command=evaluation_calculation,width=5,font=('Arial',14))
+bnt_equal.grid(row=6,column=2)
+root.mainloop()
